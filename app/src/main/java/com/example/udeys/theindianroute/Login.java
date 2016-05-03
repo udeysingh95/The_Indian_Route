5a25055ff668202d1b48afd72a325d983e89c214
Udey Singh <udeysingh95@gmaill.com>
2016-05-03 18:53:53 +0530

    Loader_init

---
+++ app/src/main/java/com/example/udeys/theindianroute/Login.java
@@ -0,0 +1,17 @@
+package com.example.udeys.theindianroute;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by udeys on 5/3/2016.
+ */
+public class Login extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+    }
+}
