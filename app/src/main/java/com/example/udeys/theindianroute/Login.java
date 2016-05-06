d6dbd6ee8093bdc2efea3699e17c784be295fefc
siddharth2848 <siddharth2848@yahoo.in>
2016-05-06 11:20:20 +0530

    Added facebook login functionality

--- app/src/main/java/com/example/udeys/theindianroute/Login.java
+++ app/src/main/java/com/example/udeys/theindianroute/Login.java
@@ -3,15 +3,23 @@ package com.example.udeys.theindianroute;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 /**
  * Created by udeys on 5/3/2016.
@@ -26,11 +34,32 @@ public class Login extends Activity {
             AccessToken accessToken = loginResult.getAccessToken();
             Profile profile = Profile.getCurrentProfile();
             if(profile != null){
+                Toast.makeText(getApplicationContext(),profile.getName().toString(),Toast.LENGTH_SHORT).show();
                 Intent in = new Intent(getApplicationContext(),MyApplication.class);
                 String name = profile.getName().toString();
                 in.putExtra("NAME",name);
                 startActivity(in);
-            }
+            }/*
+            GraphRequestAsyncTask graphRequestAsyncTask = new GraphRequest(
+                    loginResult.getAccessToken(),
+                    //AccessToken.getCurrentAccessToken(),
+                    "//friends",
+                    null,
+                    HttpMethod.GET,
+                    new GraphRequest.Callback() {
+                        public void onCompleted(GraphResponse response) {
+                            Intent intent = new Intent(getApplicationContext(),MyApplication.class);
+                            try {
+                                //Toast.makeText(getApplicationContext(),"sending friend list",Toast.LENGTH_SHORT).show();
+                                JSONArray rawName = response.getJSONObject().getJSONArray("data");
+                                intent.putExtra("jsondata", rawName.toString());
+                                startActivity(intent);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+            ).executeAsync();*/
         }
 
         @Override
--- app/src/main/java/com/example/udeys/theindianroute/MyApplication.java
+++ app/src/main/java/com/example/udeys/theindianroute/MyApplication.java
@@ -3,8 +3,18 @@ package com.example.udeys.theindianroute;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 /**
  * Created by Siddharth Malhotra on 5/4/2016.
@@ -21,7 +31,32 @@ public class MyApplication extends AppCompatActivity{
             Intent in = getIntent();
             Toast.makeText(getApplicationContext(),"Welcome: " +in.getStringExtra("NAME"),Toast.LENGTH_SHORT).show();
         }catch (Exception e){
+            Toast.makeText(getApplicationContext(),"Error: " +e.getMessage(),Toast.LENGTH_SHORT).show();
+        }/*
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myapplication);
+
+        Intent intent = getIntent();
+        String jsondata = intent.getStringExtra("jsondata"); // receiving data from  MainActivity.java
+
+        JSONArray friendslist;
+        ArrayList<String> friends = new ArrayList<>();
+        try {
+            friendslist = new JSONArray(jsondata);
+            for (int l=0; l < friendslist.length(); l++) {
+                friends.add(friendslist.getJSONObject(l).getString("name"));
+                Toast.makeText(getApplicationContext(),"name: " + friends.get(l),Toast.LENGTH_SHORT).show();
+            }
+        } catch (JSONException e) {
+            Toast.makeText(getApplicationContext(),"Error: " +e.getMessage(),Toast.LENGTH_SHORT).show();
         }
+
+        // adapter which populate the friends in listview
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_listview, friends);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);*/
+
     }
 }
