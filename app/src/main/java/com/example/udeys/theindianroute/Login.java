72967aea140eefc93892ece63f2a372bc8ea1eba
Udey Singh <udeysingh95@gmaill.com>
2016-06-09 16:18:45 +0530

    login!

--- app/src/main/java/com/example/udeys/theindianroute/Login.java
+++ app/src/main/java/com/example/udeys/theindianroute/Login.java
@@ -1,8 +1,14 @@
 package com.example.udeys.theindianroute;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -14,20 +20,40 @@ import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Created by udeys on 5/3/2016.
  */
 
 public class Login extends Activity {
+    Button log;
+    EditText username, passsword;
+    String user, pass;
+    private ProgressDialog pDialog;
 
     private CallbackManager callbackManager = null;
-    private FacebookCallback <LoginResult> callback = new FacebookCallback<LoginResult>() {
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
             AccessToken accessToken = loginResult.getAccessToken();
             Profile profile = Profile.getCurrentProfile();
-            if(profile != null){
-                Toast.makeText(getApplicationContext(),profile.getName().toString(),Toast.LENGTH_SHORT).show();
+            if (profile != null) {
+                Toast.makeText(getApplicationContext(), profile.getName().toString(), Toast.LENGTH_SHORT).show();
                 Intent in = new Intent(getApplicationContext(), Profile.class);
                 String name = profile.getName().toString();
                 String userId = profile.getId().toString();
@@ -67,23 +93,116 @@ public class Login extends Activity {
 
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.login);
-        callbackManager = CallbackManager.Factory.create();
+        username = (EditText) findViewById(R.id.username);
+        passsword = (EditText) findViewById(R.id.password);
+        log = (Button) findViewById(R.id.login);
+        user = username.getText().toString();
+        pass = passsword.getText().toString();
 
+        callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
-        loginButton.registerCallback(callbackManager,callback);
-
+        loginButton.registerCallback(callbackManager, callback);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pDialog.setMessage("Please Wait..");
+                showDialog();
+                LoginTask l = new LoginTask();
+                l.execute(user, pass);
+            }
+        });
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode,resultCode,data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
+    class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String username, password;
+            username = params[0];
+            password = params[1];
+            String serverURL = "http://indianroute.roms4all.com/login.php";
+
+            InputStream is = null;
+
+            List<NameValuePair> nameValuePairs = new ArrayList<>();
+            nameValuePairs.add(new BasicNameValuePair("username", username));
+            nameValuePairs.add(new BasicNameValuePair("password", password));
+
+
+            String result = null;
+
+            try {
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(serverURL);
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpClient.execute(httpPost);
+                HttpEntity entity = response.getEntity();
+                is = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                result = sb.toString();
+
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            String s = result.trim();
+            if (s.equalsIgnoreCase("success")) {
+                hideDialog();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Invalid Email", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+        }
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing()) {
+            pDialog.show();
+        }
+    }
+
+    private void hideDialog() {
+        if (!pDialog.isShowing()) {
+            pDialog.dismiss();
+        }
+    }
+
 }
