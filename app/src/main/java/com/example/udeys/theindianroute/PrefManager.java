4f66a024e3f130e838d29a721e61896ccc074280
siddharth2848 <siddharth2848@yahoo.in>
2016-06-18 12:55:17 +0530

    Camera API added

--- app/src/main/java/com/example/udeys/theindianroute/PrefManager.java
+++ app/src/main/java/com/example/udeys/theindianroute/PrefManager.java
@@ -1,23 +1,21 @@
 package com.example.udeys.theindianroute;
 
-/**
- * Created by Gitesh on 27-05-2016.
- */
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/**
+ * Created by Gitesh on 27-05-2016.
+ */
+
 public class PrefManager {
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
     SharedPreferences pref;
     SharedPreferences.Editor editor;
     Context _context;
-
     // shared pref mode
     int PRIVATE_MODE = 0;
-
-    // Shared preferences file name
-    private static final String PREF_NAME = "androidhive-welcome";
-
-    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
 
     public PrefManager(Context context) {
         this._context = context;
@@ -25,13 +23,13 @@ public class PrefManager {
         editor = pref.edit();
     }
 
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
     public void setFirstTimeLaunch(boolean isFirstTime) {
         editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
         editor.commit();
-    }
-
-    public boolean isFirstTimeLaunch() {
-        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
     }
 
 }
