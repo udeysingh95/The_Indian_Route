bb499f754789af7f53741746fc883210018927ff
giteshk11 <writemail2gitesh@gmail.com>
2016-06-22 16:51:41 +0530

    Commit v0.35 Comment Started

--- app/src/main/java/com/example/udeys/theindianroute/helperClasses/comments.java
+++ app/src/main/java/com/example/udeys/theindianroute/helperClasses/comments.java
@@ -4,4 +4,27 @@ package com.example.udeys.theindianroute.helperClasses;
  * Created by Gitesh on 22-06-2016.
  */
 public class comments {
+    String username, comments;
+
+    comments(String username, String comments) {
+        this.setUsername(username);
+        this.setComments(comments);
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 }
