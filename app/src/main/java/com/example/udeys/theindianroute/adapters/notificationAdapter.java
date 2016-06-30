e067d35e4d6294b33eca2ddf406c040fa7a6b295
giteshk11 <writemail2gitesh@gmail.com>
2016-06-30 15:06:39 +0530

    notification Commit v0.1

--- app/src/main/java/com/example/udeys/theindianroute/adapters/notificationAdapter.java
+++ app/src/main/java/com/example/udeys/theindianroute/adapters/notificationAdapter.java
@@ -3,14 +3,33 @@ package com.example.udeys.theindianroute.adapters;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Gitesh on 30-06-2016.
  */
 public class notificationAdapter extends ArrayAdapter {
+    List list = new ArrayList();
 
     public notificationAdapter(Context context, int resource) {
         super(context, resource);
     }
 
+    @Override
+    public void add(Object object) {
+        super.add(object);
+        list.add(object);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
 
 }
