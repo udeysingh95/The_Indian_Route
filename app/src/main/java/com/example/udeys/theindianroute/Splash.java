83e7b288dfc1f5b08f693624aa5f54a4bfac65c1
siddharth2848 <siddharth2848@yahoo.in>
2016-07-04 14:02:49 +0530

    bugs fixes and improvements

--- app/src/main/java/com/example/udeys/theindianroute/Splash.java
+++ app/src/main/java/com/example/udeys/theindianroute/Splash.java
@@ -22,6 +22,7 @@ public class Splash extends Activity {
     private final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 2;
     private final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 3;
     private final int MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE_WRITE = 4;
+    private final int MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE_READ = 5;
     ImageView im1, im2, im3, im4, im5;
 
     @Override
@@ -83,6 +84,24 @@ public class Splash extends Activity {
             }
 
             case MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE_WRITE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    Toast.makeText(getApplicationContext(), "GRANTED", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            case MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE_READ: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -186,6 +205,27 @@ public class Splash extends Activity {
                 // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE_WRITE);
+
+                // MY_PERMISSIONS_REQUEST_CAMERA is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE_READ);
 
                 // MY_PERMISSIONS_REQUEST_CAMERA is an
                 // app-defined int constant. The callback method gets the
