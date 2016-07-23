97f846922f458b5c96c4ecf2711ad081a9cde12b
Udey Singh <udeysingh95@gmaill.com>
2016-07-23 17:13:08 +0530

    MenuActivity Commit v1.1! Design Test 1.1!

--- app/src/main/java/com/example/udeys/theindianroute/MenuActivity.java
+++ app/src/main/java/com/example/udeys/theindianroute/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.udeys.theindianroute;
 
+import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,6 +13,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.udeys.theindianroute.TheIndianRoute.IndianRoute;
@@ -40,6 +43,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton hm, trp, pst, notif, prfl;      //menuBar
     ImageButton Logo, Search;
     EditText srch;
+    TextView title;
     Boolean search_state = false;
     FragmentTransaction ft;
     Intent intent = null;
@@ -62,6 +66,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         alert = (LinearLayout) findViewById(R.id.alert_button);
         passport = (LinearLayout) findViewById(R.id.passport_button);
 
+        title = (TextView)findViewById(R.id.title);
         SharedPreferences sp = getApplicationContext().getSharedPreferences("user_details", MODE_PRIVATE);
         String username = sp.getString("username", null);
         user_id = sp.getString("user_id", null);
@@ -114,11 +119,11 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int id = v.getId();
-
-        switch (id) {
+       switch (id) {
             case R.id.home:
                 reduce_alpha();
                 home.setAlpha(1f);
+                title.setText("home");
                 ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_1, new HomeFragment());
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
@@ -127,6 +132,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.trip:
                 reduce_alpha();
                 trip.setAlpha(1f);
+                title.setText("trip mode");
                 ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_1, new TripFragment());
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
@@ -134,7 +140,6 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.post:
                 reduce_alpha();
-                post.setAlpha(1f);
                 ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_1, new CameraFragment());
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
@@ -143,6 +148,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.notification:
                 reduce_alpha();
                 alert.setAlpha(1f);
+                title.setText("alerts");
                 ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_1, new NotificationFragment());
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
@@ -151,6 +157,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.profile:
                 reduce_alpha();
                 passport.setAlpha(1f);
+                title.setText("passport");
                 ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.fragment_1, new ProfileFragment());
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
@@ -163,11 +170,13 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_search:
                 if (search_state == false) {
                     Search.setImageResource(R.drawable.ic_action_back);
+                    title.setVisibility(View.GONE);
                     srch.setVisibility(View.VISIBLE);
                     search_state = true;
                 } else {
                     Search.setImageResource(R.drawable.ic_action_search_dark);
-                    srch.setVisibility(View.INVISIBLE);
+                    srch.setVisibility(View.GONE);
+                    title.setVisibility(View.VISIBLE);
                     search_state = false;
                 }
                 break;
@@ -243,7 +252,6 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     private void reduce_alpha(){
         home.setAlpha(0.5f);
         trip.setAlpha(0.5f);
-        post.setAlpha(0.5f);
         alert.setAlpha(0.5f);
         passport.setAlpha(0.5f);
     }
